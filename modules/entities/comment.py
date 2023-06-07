6def64191b9308bbc040ff3938e4c8221228e8ee
Niwaiwa <po2846@gmail.com>
2023-06-07 23:58:21 +0800

    update comment entity and repository
    change comment entity to use relation property
    update comment repository

--- modules/entities/comment.py
+++ modules/entities/comment.py
@@ -1,8 +1,8 @@
 
 class CommentEntity:
-    def __init__(self, id, profile, user, content, created_at, updated_at, parent_comment):
+    def __init__(self, id, relation, user, content, created_at, updated_at, parent_comment):
         self.id = id
-        self.profile = profile
+        self.relation = relation
         self.user = user
         self.content = content
         self.created_at = created_at
@@ -10,8 +10,8 @@ class CommentEntity:
         self.parent_comment = parent_comment
 
     def __repr__(self):
-        return "<CommentEntity id:%s profile:%s user:%s content:%s created_at:%s updated_at:%s parent_comment:%s>" % (
-            self.id, self.profile, self.user, self.content, self.created_at, self.updated_at, self.parent_comment)
+        return "<CommentEntity id:%s relation:%s user:%s content:%s created_at:%s updated_at:%s parent_comment:%s>" % (
+            self.id, self.relation, self.user, self.content, self.created_at, self.updated_at, self.parent_comment)
 
     def __str__(self):
         return self.__repr__()
--- modules/repository/comment_repository.py
+++ modules/repository/comment_repository.py
@@ -2,37 +2,69 @@ from .base_repository import BaseRepository
 from typing import Any, Dict, List
 from django.db import models
 from modules.entities.comment import CommentEntity
+from typing import Optional
 
 class CommentRepository(BaseRepository):
+    relation_property_map = {
+        'VideoComment': 'video',
+        'ImageSlideComment': 'slide',
+        'PostComment': 'post',
+        'ProfileComment': 'profile',
+    }
+
     def __init__(self, model_class: models.Model) -> None:
         self.model_class = model_class
+        self.relation_property = self.relation_property_map[model_class.__name__.lower()]
 
-    def get(self, id: str) -> models.Model:
-        return self.model_class.objects.get(id=id)
+    def get(self, id: str) -> Optional[CommentEntity]:
+        comment = self.model_class.objects.filter(id=id).first()
+        if comment is None:
+            return None
+        return self._get_comment_entity(comment)
     
-    def filter(self, **kwargs) -> models.Model:
-        return self.model_class.objects.filter(**kwargs)
+    def filter(self, **kwargs) -> Optional[CommentEntity]:
+        comments = self.model_class.objects.filter(**kwargs)
+        if len(comments) > 1:
+            return self._get_multi_comment_entity(comments)
+        elif len(comments) == 1:
+            comment = comments[0]
+            return self._get_comment_entity(comment)
+        else:
+            return None
 
-    def all(self) -> List[models.Model]:
-        return self.model_class.objects.all()
+    def all(self) -> List[Optional[CommentEntity]]:
+        comments = self.model_class.objects.all()
+        if len(comments) > 1:
+            return self._get_multi_comment_entity(comments)
+        elif len(comments) == 1:
+            comment = comments[0]
+            return self._get_comment_entity(comment)
+        else:
+            return None
 
-    def create(self, data: Dict[str, Any]) -> CommentEntity:
+    def create(self, data: Dict[str, Any]) -> Optional[CommentEntity]:
         model = self.model_class(**data)
         model.save()
+        return self._get_comment_entity(model)
+
+    def update_one(self, data: Dict[str, Any]) -> Optional[CommentEntity]:
+        model = self.model_class(**data)
+        model.save()
+        return self._get_comment_entity(model)
+
+    def delete(self, id: str) -> None:
+        model = self.model_class.objects.get(id=id)
+        model.delete()
+
+    def _get_comment_entity(self, model: models.Model) -> CommentEntity:
         return CommentEntity(
             id=model.id,
-            profile=model.profile,
+            relation=getattr(model, self.relation_property),
             user=model.user,
             content=model.content,
             created_at=model.created_at,
             updated_at=model.updated_at,
             parent_comment=model.parent_comment)
-
-    def update(self, data: Dict[str, Any]) -> None:
-        model = self.model_class(**data)
-        data.pop('id')
-        self.model_class.objects.bulk_update([model], data.keys())
-
-    def delete(self, id: str) -> None:
-        model = self.model_class.objects.get(id=id)
-        model.delete()
+    
+    def _get_multi_comment_entity(self, models: List[models.Model]) -> List[Optional[CommentEntity]]:
+        return [self._get_comment_entity(model) for model in models]
